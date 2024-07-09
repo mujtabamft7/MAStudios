7a2e5aa77258185e721537a65ff03a8337cdb75a
Mujtaba Althuwaynee <mujtabamft@gmail.com>
2024-07-08 21:27:45 -0400

    forth deployment test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <footer className="footer">
