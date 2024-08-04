8093b9a039ad27089e5e6139b53587722b6702a7
Mujtaba Althuwaynee <mujtabamft@gmail.com>
2024-08-04 15:29:06 -0400

    new test

--- src/Axios.js
+++ src/Axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true; 
+
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://video-backend-y112.onrender.com/api';
 
 // Create an Axios instance with CORS configuration
