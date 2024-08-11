e00766759af1c7e5ff64d11dbc45dc871b26f32c
Mujtaba Malik Al-Thuwaynee <mujtabamft@gmail.com>
2024-08-10 20:39:33 -0400

    Update Axios.js

--- src/Axios.js
+++ src/Axios.js
@@ -4,7 +4,7 @@ axios.defaults.withCredentials = true;
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://video-backend-y112.onrender.com/api';
 
-// Create an Axios instance with CORS configuration
+
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
